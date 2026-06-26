a3f0fccffebf80983165642f3a9733653b7b1c7e
Mehak-gupta23 <mehakgupta811@gmail.com>
2026-06-26 23:41:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const photos = [
-    "images/photo1.jpg",
-    "images/photo2.jpg",
-    "images/photo3.jpg",
-    "images/photo4.jpg"
+    "images/photo1.jpeg",
+    "images/photo2.jpeg",
+    "images/photo3.jpeg",
+    "images/photo4.jpeg"
 ];
 
 let current = 0;
